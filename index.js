2a2553258f03661be94cb91fc3c48ad94b2225e3
Naeem676 <ali.naeem939@gmail.com>
2021-07-16 00:10:38 +0600

    'all'

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ app.use(express.static('service'));
 
 app.use(fileUpload());
 
+app.all('/secret', function (req, res, next) {
+  console.log('Accessing the secret section ...')
+  next() // pass control to the next handler
+})
+
 
 
 
